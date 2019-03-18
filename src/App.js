e334d225fcc377c02c7666bac9a57e188b4d20f7
simonedegiacomi <simonedegiacomi97@gmail.com>
2019-03-18 22:33:01 +0100

    Use local TinyMCE istad of the cloud version (to hide the warning alert)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import React, {Component} from 'react';
 import Layout from './Components/Layout'
 import './App.css';
 
+/* Initialize TinyMCE */
+// Import TinyMCE
+import tinymce from 'tinymce/tinymce';
+
+// A theme is also required
+import 'tinymce/themes/silver/theme';
+
+// Any plugins you want to use has to be imported
+import 'tinymce/plugins/paste';
+import 'tinymce/plugins/link';
+
+// Initialize the app
+tinymce.init({
+  selector: '#tiny',
+  plugins: ['paste', 'link']
+});
+/* End of TinyMCE initialization */
 
 
 class App extends Component {
