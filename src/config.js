5481394b9bb98616bc31a04ce17d696bbcf72399
simonedegiacomi <simonedegiacomi97@gmail.com>
2019-06-10 12:04:35 +0200

    fix public workflow link generation

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const WEBAPP_URL = process.env.REACT_APP_SERVER_URL || "http://localhost:4000";
+export const WEBAPP_URL = process.env.REACT_WEBAPP_URL || "http://localhost:4000";
 export const API_URL = process.env.REACT_APP_SERVER_URL || "http://localhost:3000";
