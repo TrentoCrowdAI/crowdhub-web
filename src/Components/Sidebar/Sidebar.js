22e3d62c544b2cd4e64c2c725f4da2a48a640231
Jorge Ramírez <jorgeramirez1990@gmail.com>
2019-10-21 12:37:20 +0200

    Update sidebar label
    Signed-off-by: Jorge Ramírez <jorgeramirez1990@gmail.com>

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,13 +1,12 @@
-import React, {Component} from "react";
-import {NavLink} from "react-router-dom";
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import './Sidebar.css';
-import RunsService from "../../Services/rest/RunsService";
-import {WORKFLOWS_PATH} from "../Workflows/Workflows";
+import RunsService from '../../Services/rest/RunsService';
+import { WORKFLOWS_PATH } from '../Workflows/Workflows';
 
 const MAX_RUNS_IN_LIST = 5;
 
 export default class Sidebar extends Component {
-
   state = {
     runs: null
   };
@@ -17,7 +16,7 @@ export default class Sidebar extends Component {
   async fetchRuns() {
     try {
       const runs = await RunsService.getRuns();
-      this.setState({runs});
+      this.setState({ runs });
     } catch (e) {
       // TODO: Handle error
     }
@@ -26,21 +25,19 @@ export default class Sidebar extends Component {
   render() {
     return (
       <nav id="sidebar" className={this.props.sidebarOpen ? 'open' : ''}>
-        <h2 className="app-title">Servant</h2>
+        <h2 className="app-title">CrowdHub</h2>
 
         <ul className="sidebar-list">
           <li>
-            <NavLink to="/projects">
-              Projects
-            </NavLink>
+            <NavLink to="/projects">Projects</NavLink>
           </li>
 
           <li>
-            <LatestFinishedRunsList runs={this.state.runs}/>
+            <LatestFinishedRunsList runs={this.state.runs} />
           </li>
 
           <li>
-            <RunningRunsList runs={this.state.runs}/>
+            <RunningRunsList runs={this.state.runs} />
           </li>
         </ul>
       </nav>
@@ -48,43 +45,38 @@ export default class Sidebar extends Component {
   }
 }
 
-
-const LatestFinishedRunsList = ({runs}) => {
+const LatestFinishedRunsList = ({ runs }) => {
   if (!runs) {
     return <p>Fetching latest finished runs ...</p>;
   }
   return (
     <div>
       Latest finished runs
-      <JobsList runs={runs.getFinishedRuns().slice(0, MAX_RUNS_IN_LIST)}/>
+      <JobsList runs={runs.getFinishedRuns().slice(0, MAX_RUNS_IN_LIST)} />
     </div>
   );
 };
 
-
-const RunningRunsList = ({runs}) => {
+const RunningRunsList = ({ runs }) => {
   if (!runs) {
     return <p>Fetching running runs ...</p>;
   }
   return (
     <div>
       Running runs
-      <JobsList runs={runs.getRunningRuns().slice(0, MAX_RUNS_IN_LIST)}/>
+      <JobsList runs={runs.getRunningRuns().slice(0, MAX_RUNS_IN_LIST)} />
     </div>
   );
 };
 
-
-const JobsList = ({runs}) => (
+const JobsList = ({ runs }) => (
   <ul>
-    {
-      runs.map(run => (
-        <li key={run.id}>
-          <NavLink to={`${WORKFLOWS_PATH}/${run.getWorkflowId()}`}>
-            #{run.id}
-          </NavLink>
-        </li>
-      ))
-    }
+    {runs.map(run => (
+      <li key={run.id}>
+        <NavLink to={`${WORKFLOWS_PATH}/${run.getWorkflowId()}`}>
+          #{run.id}
+        </NavLink>
+      </li>
+    ))}
   </ul>
 );
