f40f71fb47e872f52d4fd5b7be18b0d83a27d9c4
Jorge Ramírez <jorgeramirez1990@gmail.com>
2019-10-22 10:42:04 +0200

    Fix F8 label in account information
    Signed-off-by: Jorge Ramírez <jorgeramirez1990@gmail.com>

--- src/Components/Appbar/AccountInformation.js
+++ src/Components/Appbar/AccountInformation.js
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';
-import {OverlayTrigger, Popover} from "react-bootstrap";
-import "./AccountInformation.css";
-import {makeCancellable} from "../../Services/rest/utils";
-import AccountBalanceService from "../../Services/rest/AccountBalanceService";
-import LoadingContainer from "../common/LoadingContainer";
-import AuthService from "../../Services/AuthService";
-import LoadingButton from "../common/LoadingButton";
-import {LOGIN_PATH} from "../Login/Login";
-import {Redirect} from "react-router-dom";
+import React, { Component } from 'react';
+import { OverlayTrigger, Popover } from 'react-bootstrap';
+import './AccountInformation.css';
+import { makeCancellable } from '../../Services/rest/utils';
+import AccountBalanceService from '../../Services/rest/AccountBalanceService';
+import LoadingContainer from '../common/LoadingContainer';
+import AuthService from '../../Services/AuthService';
+import LoadingButton from '../common/LoadingButton';
+import { LOGIN_PATH } from '../Login/Login';
+import { Redirect } from 'react-router-dom';
 
 export default class AccountInformation extends Component {
   render() {
@@ -16,14 +16,12 @@ export default class AccountInformation extends Component {
         placement="bottom"
         trigger="click"
         overlay={
-          <Popover id="account-information-popover"
-                   title="Account information">
-            <AccountInformationPopoverContent/>
+          <Popover id="account-information-popover" title="Account information">
+            <AccountInformationPopoverContent />
           </Popover>
         }>
-
         <div>
-          <AccountInformationPreview/>
+          <AccountInformationPreview />
         </div>
       </OverlayTrigger>
     );
@@ -36,30 +34,31 @@ class AccountInformationPreview extends Component {
     return (
       <div>
         <span>{userInfo.getName()}</span>
-        <img className="user-avatar" src={userInfo.getImageUrl()} alt="user avatar"/>
+        <img
+          className="user-avatar"
+          src={userInfo.getImageUrl()}
+          alt="user avatar"
+        />
       </div>
     );
   }
 }
-
 
 export class AccountInformationPopoverContent extends Component {
   render() {
     return (
       <div>
-        <PlatformBalances/>
+        <PlatformBalances />
 
-        <hr/>
+        <hr />
 
-        <LogoutButton/>
+        <LogoutButton />
       </div>
     );
   }
 }
 
-
 export class PlatformBalances extends Component {
-
   pendingBalancesRequest;
 
   state = {
@@ -72,13 +71,15 @@ export class PlatformBalances extends Component {
   componentWillUnmount = () => this.pendingBalancesRequest.cancel();
 
   fetchBalances = async () => {
-    this.setState({balances: null, fetchError: false});
+    this.setState({ balances: null, fetchError: false });
     try {
-      this.pendingBalancesRequest = makeCancellable(AccountBalanceService.getBalances());
+      this.pendingBalancesRequest = makeCancellable(
+        AccountBalanceService.getBalances()
+      );
       const balances = await this.pendingBalancesRequest.result;
-      this.setState({balances, fetchError: false});
+      this.setState({ balances, fetchError: false });
     } catch (e) {
-      this.setState({balances: null, fetchError: true});
+      this.setState({ balances: null, fetchError: true });
     }
   };
 
@@ -87,54 +88,52 @@ export class PlatformBalances extends Component {
       <div className="balances-container">
         <h6>Balances</h6>
 
-        <LoadingContainer loading={!this.state.balances && !this.state.fetchError}>
-          {
-            !this.state.balances && this.state.fetchError &&
-            <p>Error</p>
-          }
+        <LoadingContainer
+          loading={!this.state.balances && !this.state.fetchError}>
+          {!this.state.balances && this.state.fetchError && <p>Error</p>}
 
-          {
-            this.state.balances && !this.state.fetchError &&
-            this.renderBalances()
-          }
+          {this.state.balances &&
+            !this.state.fetchError &&
+            this.renderBalances()}
         </LoadingContainer>
       </div>
     );
   }
 
-  static prepareNumber = (balance) => typeof balance === "number" ? balance.toFixed(2) : "not available";
+  static prepareNumber = balance =>
+    typeof balance === 'number' ? balance.toFixed(2) : 'not available';
 
   renderBalances = () => {
-    const {tolokaSandbox, tolokaNormal, f8} = this.state.balances;
+    const { tolokaSandbox, tolokaNormal, f8 } = this.state.balances;
     return (
       <div>
-        <strong>Toloka Sandbox</strong>: {PlatformBalances.prepareNumber(tolokaSandbox)}$<br/>
-        <strong>Toloka</strong>: {PlatformBalances.prepareNumber(tolokaNormal)}$<br/>
-        <strong>Toloka</strong>: {PlatformBalances.prepareNumber(f8)}$<br/>
+        <strong>Toloka Sandbox</strong>:{' '}
+        {PlatformBalances.prepareNumber(tolokaSandbox)}$<br />
+        <strong>Toloka</strong>: {PlatformBalances.prepareNumber(tolokaNormal)}$
+        <br />
+        <strong>Figure Eight</strong>: {PlatformBalances.prepareNumber(f8)}$
+        <br />
       </div>
     );
   };
-
 }
 
-
 class LogoutButton extends Component {
-
   state = {
     loggingOut: false,
     loggedOut: false
   };
 
   onLogout = async () => {
-    this.setState({loggingOut: true});
+    this.setState({ loggingOut: true });
     try {
       await AuthService.signOut();
-      this.setState({loggedOut: true});
+      this.setState({ loggedOut: true });
     } catch (e) {
       console.error(e);
       // TODO: Handle error
     }
-    this.setState({loggingOut: false});
+    this.setState({ loggingOut: false });
   };
 
   render() {
@@ -142,11 +141,14 @@ class LogoutButton extends Component {
       return this.renderRedirect();
     }
     return (
-      <LoadingButton isLoading={this.state.loggingOut} onClick={this.onLogout} block>Logout</LoadingButton>
+      <LoadingButton
+        isLoading={this.state.loggingOut}
+        onClick={this.onLogout}
+        block>
+        Logout
+      </LoadingButton>
     );
   }
 
-  renderRedirect = () => (
-    <Redirect to={LOGIN_PATH}/>
-  );
+  renderRedirect = () => <Redirect to={LOGIN_PATH} />;
 }
